ddc5140c2af13b47b96e23f01ddf2d25fcb24ea9
Nash-Cash <45920023+Nash-Cash@users.noreply.github.com>
2019-01-19 11:10:04 +0100

    del

--- linux/external/cryptopp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/new/external/cryptopp
-# Build directory: /home/daniel/new/linux/external/cryptopp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
