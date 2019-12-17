5b0f6c8300d610e4be2d738819558ce715153f7c
Leinad2k <45920023+Nash-Cash@users.noreply.github.com>
2019-12-17 18:17:05 +0100

    ops

--- out/build/x64-Debug/build_version.cc
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-#include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@0@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2019/12/15 16:12:48@";
-const char* rocksdb_build_compile_date = __DATE__;
--- out/build/x64-Debug/external/cryptopp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/evo/evo/external/cryptopp
-# Build directory: D:/evo/evo/out/build/x64-Debug/external/cryptopp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
